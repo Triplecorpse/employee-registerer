2032362798f48bfb8121b477188b40e66b7641aa
ksushantonova <masushantonova@gmail.com>
2019-11-12 11:57:07 +0200

    take id from card

--- serial-test.ts
+++ serial-test.ts
@@ -3,10 +3,42 @@ import { Serial } from 'raspi-serial';
  
 init(() => {
   const serial = new Serial();
+  const parts: Array<Buffer> = [];
+  let buffer: Buffer;
+  let read: string;
+
   serial.open(() => {
+    console.log('Place you RFID card');
+
     serial.on('data', (data) => {
-      process.stdout.write(data);
+      parts.push(data);
+      
+      if (parts.length === 2) {
+        const tempBuffer = Buffer.concat(parts);
+        let tempRead = tempBuffer.toString();
+        parts.length = 0;
+
+        if (read !== tempRead) {
+          buffer = tempBuffer;
+          read = tempRead;
+         
+          detailed(buffer);
+        }
+
+        setTimeout(() => {tempRead = ''}, 1000);
+      }
     });
-    serial.write('Hello from raspi-serial');
   });
 });
+
+function detailed(buff: Buffer) {
+  const tag = parseInt(buff.slice(3, 11).toString(), 16).toString(10);
+  const zeroes = 10 - tag.length;
+
+  //console.log('total', buff)
+  //console.log('head', buff.slice(0, 1));
+  //console.log('version', buff.slice(1, 3).toString());
+  console.log('tag', '0'.repeat(zeroes) + tag);
+  //console.log('checksum', buff.slice(11, 13).toString());
+  //console.log('tail', buff.slice(13))
+}
